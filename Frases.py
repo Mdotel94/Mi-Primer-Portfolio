b63074cf3a17da2a61a1171d59549b37d0db21f1
maria <mariajosedotel@gmail.com>
2025-03-26 12:10:08 +0100

    Contador de palabras

---
+++ Frases.py
@@ -0,0 +1,11 @@
+#Pide al usuario que introduzca palabras
+frase = input("Introduce una frase: ")
+
+#Split divide cada palabra usando espacio como separador
+palabras = frase.split()
+
+#len sirve para contar cada palabra
+numero_de_palabras = len(palabras)
+
+#Print muestra resultado a usuario.
+print(f"La frase tiene {numero_de_palabras} palabras.")
